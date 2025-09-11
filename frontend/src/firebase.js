ead569bd165cf359da9bb7d5c843125bd4f4069f
binukhewage <binukhe@gmail.com>
2025-09-11 23:30:10 +0530

    updated paths

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -19,13 +19,13 @@ import {
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCoamvgAPhmChUsyGwbRlF6_Otibyrtfns",
-  authDomain: "supunexpress-e0654.firebaseapp.com",
-  projectId: "supunexpress-e0654",
-  storageBucket: "supunexpress-e0654.firebasestorage.app",
-  messagingSenderId: "137995043873",
-  appId: "1:137995043873:web:5c2c472e7a53797adf0f60",
-  measurementId: "G-GRGV4CX7LV",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
